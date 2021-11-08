0ee458b1558fddf58e8f2690671a5a34ed026075
chris roughley <chris.roughley@ntlworld.com>
2021-11-08 17:19:08 +0000

    add presence handler

--- src/utils/firebaseDatabaseUtils.js
+++ src/utils/firebaseDatabaseUtils.js
@@ -4,38 +4,26 @@ import {
   ref,
   set,
   serverTimestamp,
+  push,
 } from "firebase/database";
 import { database } from "./firebase";
 import { auth } from "./firebase";
 
 export const handlePresence = () => {
   const uid = auth.currentUser.uid;
-
-  const userStatusRef = ref(database, "status/" + uid);
-
-  const isOfflineForDatabase = {
-    state: "offline",
-    last_changed: serverTimestamp(),
-  };
-
-  const isOnlineForDatabase = {
-    state: "online",
-    last_changed: serverTimestamp(),
-  };
-
-  set(userStatusRef, isOnlineForDatabase);
-
+  const userConnectionsRef = ref(database, "status/" + uid + "/connections");
+  const lastOnlineRef = ref(database, "status/" + uid + "/lastOnline");
   const infoConnectedRef = ref(database, ".info/connected");
 
   onValue(infoConnectedRef, async (snapshot) => {
-    if (snapshot.val() === false) {
-      return;
-    }
+    if (snapshot.val() === false) return;
 
-    onDisconnect(userStatusRef)
-      .set(isOfflineForDatabase)
-      .then(() => {
-        set(userStatusRef, isOnlineForDatabase);
-      });
+    const connectionRef = push(userConnectionsRef);
+
+    onDisconnect(connectionRef).remove();
+
+    set(connectionRef, true);
+
+    onDisconnect(lastOnlineRef).set(serverTimestamp());
   });
 };
